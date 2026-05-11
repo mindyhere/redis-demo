c2de9487093ed3f79abe89574ac5f894ff8cd12c
mindy <92miindy@gmail.com>
2026-05-11 14:44:32 +0900

    [refactor] repository 패키지 분리

--- src/main/java/com/example/redis/repository/ItemRepository.java
+++ src/main/java/com/example/redis/repository/ItemRepository.java
@@ -1,6 +1,7 @@
-package com.example.redis;
+package com.example.redis.repository;
 
 
+import com.example.redis.Item;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ItemRepository extends CrudRepository<Item, String> {
