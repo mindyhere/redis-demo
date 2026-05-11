c53ed67fbbdcadc95cb9640c74c80e4e55587c91
mindy <92miindy@gmail.com>
2026-05-11 14:21:43 +0900

    [init] redis 실습 프로젝트 생

---
+++ src/test/java/com/example/redis/RedisRepositoryTests.java
@@ -0,0 +1,44 @@
+package com.example.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RedisRepositoryTests {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    public void createTest() {
+        // 객체를 만들고
+        Item item = Item.builder()
+            .id(1L)
+            .name("keyboard")
+            .description("Very Expensive Keyboard")
+            .price(100000)
+            .build();
+
+        // save 호출
+        itemRepository.save(item);
+    }
+
+    @Test
+    public void readOneTest() {
+        Item item = itemRepository.findById(1L).orElseThrow();
+        System.out.println(item.getDescription());
+    }
+
+    @Test
+    public void updateTest() {
+        Item item = itemRepository.findById(1L).orElseThrow();
+        item.setDescription("##### ON SALE");
+        item = itemRepository.save(item);
+        System.out.println(item.getDescription());
+    }
+
+    @Test
+    public void deleteTest() {
+        itemRepository.deleteById(1L);
+    }
+}
