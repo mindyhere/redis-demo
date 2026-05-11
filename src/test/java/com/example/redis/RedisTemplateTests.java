fefb812ce2fc3bc31326209d330cade157d61d2b
mindy <92miindy@gmail.com>
2026-05-11 14:42:11 +0900

    [edit] RedisTemplate 테스트

---
+++ src/test/java/com/example/redis/RedisTemplateTests.java
@@ -0,0 +1,36 @@
+package com.example.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+public class RedisTemplateTests {
+    @Autowired
+    private StringRedisTemplate redisTemplate; // <- Redis 의 자료형(String)을 의미하는 것 X. Java에서의 자료형
+
+    @Test
+    public void stringOpsTest() {
+        // ValueOperations: 문자열 조작을 위한 클래스
+        // RedisTemplate 에 설정된 타입을 바탕으로 Redis 문자열 조작
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+
+        ops.set("simpleKey", "simpleValue1");
+        System.out.println(ops.get("simpleKey"));
+
+        // 집합을 조작하기 위한 클래스
+        SetOperations<String, String> setOps = redisTemplate.opsForSet();
+        setOps.add("hobbies", "game");
+        setOps.add("hobbies", "coding", "coffee", "swimming");
+
+        System.out.println("SCARD hobbies ##### " + setOps.size("hobbies"));
+
+        redisTemplate.expire("hobbies", 10, TimeUnit.SECONDS);
+        redisTemplate.delete("simpleKey");
+    }
+}
